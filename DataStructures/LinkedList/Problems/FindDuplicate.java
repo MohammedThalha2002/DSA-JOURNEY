95a3dda6d1fcf7884a6c911fb866e9cdd3cab2b8
MohammedThalha2002 <mohammedalithalha2002@gmail.com>
2023-10-28 21:43:51 +0530

    commit

---
+++ DataStructures/LinkedList/Problems/FindDuplicate.java
@@ -0,0 +1,19 @@
+package DataStructures.LinkedList.Problems;
+
+// Input: n = 7 , array = {1, 2, 3, 1, 3, 6, 6}
+// Output: 1, 3 and 6.
+// Explanation: Duplicate element in the array are 1 , 3 and 6
+
+// Input: n = 6, array = {5, 3, 1, 3, 5, 5}
+// Output: 3 and 5.
+// Explanation: Duplicate element in  the array are 3 and 5
+
+public class FindDuplicate {
+    public static void main(String[] args) {
+        // An Array contains one duplicate element and the array contains elements from 1 to N.
+        // Find the missing numnber without altering the array
+
+        int[] arr = {4,2,3,3,1,5};
+        
+    }
+}
