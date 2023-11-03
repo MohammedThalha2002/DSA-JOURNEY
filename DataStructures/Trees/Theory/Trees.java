adccc26ae899fda1a8207d8e6cf02960be5db1c0
MohammedThalha2002 <mohammedalithalha2002@gmail.com>
2023-11-03 23:09:53 +0530

    comit

---
+++ DataStructures/Trees/Theory/Trees.java
@@ -0,0 +1,55 @@
+package DataStructures.Trees.Theory;
+
+import java.util.Scanner;
+
+public class Trees {
+
+    private Node root;
+
+    public Trees() {
+
+    }
+
+    private class Node {
+        Node left;
+        Node right;
+        int value;
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+
+
+    public void populate(Scanner sc){
+        System.out.println("Enter the root node value : ");
+        int value = sc.nextInt();
+        root = new Node(value);
+
+        populate(sc, root);
+    }
+
+    public void populate(Scanner sc, Node node){
+        System.out.println("Do you want to add a Left Node for " + node.value);
+        boolean addLeft = sc.nextBoolean();
+
+        if(addLeft){
+            System.out.println("Enter the left node value : ");
+            int left = sc.nextInt();
+            node.left = new Node(left);
+            // recursion
+            populate(sc, node.left);
+        }
+
+        System.out.println("Do you want to add a Right Node for " + node.value);
+        boolean addRight = sc.nextBoolean();
+
+        if(addRight){
+            System.out.println("Enter the right node value : ");
+            int right = sc.nextInt();
+            node.right = new Node(right);
+            // recursion
+            populate(sc, node.right);
+        }
+    }
+}
