898b7e81ebaed484a96441643308b8aa6a9b50ac
MohammedThalha2002 <mohammedalithalha2002@gmail.com>
2023-11-05 10:37:39 +0530

    commit

--- DataStructures/Trees/Theory/InOrderTraversal.java
+++ DataStructures/Trees/Theory/InOrderTraversal.java
@@ -1,6 +1,8 @@
 package DataStructures.Trees.Theory;
 
 // LEFT => NODE => RIGHT
+// USES
+// 1) In BST, it visit the nodes in sorted Manner
 public class InOrderTraversal {
 
 }
--- DataStructures/Trees/Theory/PostOrderTraversal.java
+++ DataStructures/Trees/Theory/PostOrderTraversal.java
@@ -1,5 +1,8 @@
 package DataStructures.Trees.Theory;
 
+// LEFT => RIGHT => NODE
+// USES
+// 1) Deleting a binary tree
 public class PostOrderTraversal {
-    
+
 }
