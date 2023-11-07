0c331870b43595b417f3b27cd0e4f5e01189db1d
MohammedThalha2002 <mohammedalithalha2002@gmail.com>
2023-11-07 20:04:07 +0530

    commit

--- DataStructures/Trees/Theory/AVL.java
+++
@@ -1,7 +0,0 @@
-package DataStructures.Trees.Theory;
-
-public class AVL {
-    public static void main(String[] args) {
-
-    }
-}
--- DataStructures/Trees/Theory/SelfBalancingTree.java
+++
@@ -1,7 +0,0 @@
-package DataStructures.Trees.Theory;
-
-public class SelfBalancingTree {
-    public static void main(String[] args) {
-        
-    }
-}
