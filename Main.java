66fc9788e90c620df520fcb3327e9b5307b8194d
MohammedThalha2002 <mohammedalithalha2002@gmail.com>
2023-11-10 15:52:08 +0530

    commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+import java.io.File;
+import java.io.FileInputStream;
+
+public class Main {
+    public static void main(String[] args) {
+        File nfjd = new File();
+        FileInputStream stream = new FileInputStream("edjn.xls");
+    }
+}
