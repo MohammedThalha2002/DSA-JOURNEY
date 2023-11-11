b8cc270fdc2225c98048104b6505476737ad6474
MohammedThalha2002 <mohammedalithalha2002@gmail.com>
2023-11-11 08:02:19 +0530

    commit

---
+++ DataStructures/Trees/Theory/SegmentTrees.java
@@ -0,0 +1,9 @@
+package DataStructures.Trees.Theory;
+
+public class SegmentTrees {
+    public static void main(String[] args) {
+        String str = "HELLO";
+        StringBuilder sbr = new StringBuilder(str);
+        str = sbr.reverse().toString();
+    }
+}
