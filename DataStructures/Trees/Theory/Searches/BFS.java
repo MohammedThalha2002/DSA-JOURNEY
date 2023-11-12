84c6ed0a808f19e059d1cfb6aed5fdea222cf5f6
MohammedThalha2002 <mohammedalithalha2002@gmail.com>
2023-11-12 22:11:26 +0530

    commit

---
+++ DataStructures/Trees/Theory/Searches/BFS.java
@@ -0,0 +1,70 @@
+package DataStructures.Trees.Theory.Searches;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+import DataStructures.Trees.Theory.BST;
+import DataStructures.Trees.Theory.BST.Node;
+
+public class BFS {
+    public static void main(String[] args) {
+        BST bst = new BST();
+        int[] arr = { 4, 2, 6, 1, 3, 7, 5 };
+        for (int i = 0; i < arr.length; i++) {
+            bst.insert(arr[i]);
+        }
+        bst.display();
+        breadthFirstTraversal(bst.root);
+        System.out.println();
+        List<List<Integer>> res = bTraversal(bst.root);
+        System.out.println(res);
+    }
+
+    public static void breadthFirstTraversal(Node node) {
+        System.out.println("Breadth First Traversal");
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(node);
+
+        while (!queue.isEmpty()) {
+            Node popped = queue.poll();
+            System.out.print(popped.value + " -> ");
+            if (popped.left != null) {
+                queue.add(popped.left);
+            }
+            if (popped.right != null) {
+                queue.add(popped.right);
+            }
+        }
+
+    }
+
+    public static List<List<Integer>> bTraversal(Node node) {
+        List<List<Integer>> outer = new ArrayList<>();
+
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(node);
+
+        while (!queue.isEmpty()) {
+            int innerListSize = queue.size();
+            List<Integer> inner = new ArrayList<>();
+
+            for (int i = 0; i < innerListSize; i++) {
+                Node popped = queue.poll();
+                inner.add(popped.value);
+
+                if (popped.left != null) {
+                    queue.add(popped.left);
+                }
+                if (popped.right != null) {
+                    queue.add(popped.right);
+                }
+            }
+            outer.add(inner);
+
+        }
+
+        return outer;
+    }
+}
