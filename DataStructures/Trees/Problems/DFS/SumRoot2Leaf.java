8026a6fed506d6806ec8c3dfe4a4cb19d64ab045
MohammedThalha2002 <mohammedalithalha2002@gmail.com>
2023-11-22 08:47:23 +0530

    commit

--- DataStructures/Trees/Problems/DFS/SumRoot2Leaf.java
+++ DataStructures/Trees/Problems/DFS/SumRoot2Leaf.java
@@ -1,22 +1,39 @@
 package DataStructures.Trees.Problems.DFS;
 
-import DataStructures.Trees.Theory.BST;
-import DataStructures.Trees.Theory.BST.Node;
+import DataStructures.Trees.Theory.Trees;
+import DataStructures.Trees.Theory.Trees.Node;
 
 public class SumRoot2Leaf {
     public static void main(String[] args) {
-        BST bst = new BST();
-        int[] arr = { 5, 3, 6, 2, 4, 1 };
-        for (int i = 0; i < arr.length; i++) {
-            bst.insert(arr[i]);
-        }
-        bst.display();
-        int targetSum = 12;
-        boolean res = rootToLeafSum(bst.root, targetSum);
-        System.out.println("The path sum " + targetSum + (res ? " exits" : " not exists"));
+        Trees tree = new Trees();
+        tree.root = tree.createNode(4);
+        tree.root.left = tree.createNode(9);
+        tree.root.right = tree.createNode(0);
+        tree.root.left.left = tree.createNode(5);
+        tree.root.left.right = tree.createNode(1);
+        tree.display();
+        int targetSum = 1026;
+        int res = rootToLeafSum(tree.root, 0);
+        System.out.println(res);
+        System.out
+                .println("The root sum from leaf to node " + targetSum
+                        + (res == targetSum ? " exits" : " not exists"));
     }
 
-    private static boolean rootToLeafSum(Node root, int targetSum) {
-        return false;
+    private static int rootToLeafSum(Node node, int sum) {
+        if (node == null) {
+            return 0;
+        }
+
+        sum = (sum * 10) + node.value;
+
+        if (node.left == null && node.right == null) {
+            return sum;
+        }
+
+        int left = rootToLeafSum(node.left, sum);
+        int right = rootToLeafSum(node.right, sum);
+
+        return left + right;
     }
 }
